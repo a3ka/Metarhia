154a689e1f8b3eea8c9684593a4182160ed61631
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-07-02 15:38:02 +0300

    functions

--- Function/Exercises/1-random.js
+++ Function/Exercises/1-random.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const random = (min, max) => {
-  // Generate random Number between from min to max
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+	if (max === undefined) {
+		max = min
+		min = 0
+	}
+	return min + Math.floor(Math.random() * (max - min + 1));
 };
 
+console.log(random(1, 3));
 module.exports = { random };
--- Function/Exercises/2-key.js
+++ Function/Exercises/2-key.js
@@ -4,6 +4,14 @@ const generateKey = (length, possible) => {
   // Generate string of random characters
   // Use Math.random() and Math.floor()
   // See documentation at MDN
+  baseLength = possible.length;
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * baseLength);
+    key += possible[index]
+  }
+	
+  return key;
 };
 
 module.exports = { generateKey };
